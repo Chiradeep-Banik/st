ba426b58ebb994633f244b808c9a7049934cce6c
Chiradeep-Banik <chiradeep13@gmail.com>
2022-01-18 00:34:18 +0530

    Changed the color sceme

--- config.def.h
+++ config.def.h
@@ -95,45 +95,49 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#131b1b", /* black   */
+  [1] = "#1b2424", /* red     */
+  [2] = "#293636", /* green   */
+  [3] = "#3c4e4e", /* yellow  */
+  [4] = "#556c6c", /* blue    */
+  [5] = "#729191", /* magenta */
+  [6] = "#95bbbb", /* cyan    */
+  [7] = "#bcebeb", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#fd8588", /* black   */
+  [9]  = "#ef9de9", /* red     */
+  [10] = "#c7ad30", /* green   */
+  [11] = "#199594", /* yellow  */
+  [12] = "#4ba557", /* blue    */
+  [13] = "#98863d", /* magenta */
+  [14] = "#2aa942", /* cyan    */
+  [15] = "#fd9699", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+  /* special colors */
+  [256] = "#0b1111", /* background */
+  [257] = "#a7d1d1", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+
 static unsigned int defaultrcs = 257;
 
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 /*
  * Default shape of cursor
  * 2: Block ("█")
@@ -141,7 +145,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
